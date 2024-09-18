9b819fefaafb6c64e0321c8107cd855a74fde9d8
Noc-art <nevinthang02@gmail.com>
2024-09-18 13:46:54 +0700

    pws autodeploy

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://nevin-thang-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://nevin-thang-mentalhealthtracker.pbp.cs.ui.ac.id/"]
